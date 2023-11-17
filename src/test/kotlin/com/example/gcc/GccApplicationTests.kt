881598c199d62d41f5ceff27364e1775a7d94c32
eberecodes <danielleebereanukem@gmail>
2023-11-17 12:11:23 +0000

    #1 initial template kotlin

---
+++ src/test/kotlin/com/example/gcc/GccApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gcc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GccApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
